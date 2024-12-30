de2e141ff51754429d372956368171e7b3acccda
Brad Weir <45017314+briardew@users.noreply.github.com>
2024-12-30 14:09:50 -0500

    Removing site-level dependencies

--- bin/post/setup.sh
+++ bin/post/setup.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-# I'd really like to read these somewhere central
+# Run-specific settings
+# (needs serious improvement)
+# ---
 VERSION="1"
-COLTAG="MiCASA_v${VERSION}_flux_x3600_y1800"
-RESTAG="0.1 degree x 0.1 degree"
-FEXT="nc4"
+DIRIN="/discover/nobackup/bweir/MiCASA/data-casa/daily-0.1deg/holding"
+ROOTOUT="/css/gmao/geos_carb/pub"
+#VERSION="NRT"
+#DIRIN="/discover/nobackup/ghg_ops/MiCASA/data-casa/daily-0.1deg-nrt/holding"
+#ROOTOUT="/css/gmao/geos_carb/share"
 
 # I'd really like to send these as commands
 REPRO=false
 REPROCOG=false
 
-# System-specific settings
+# The rest should auto-generate
 # ---
-# 1. Input
-# Fancy way to point MIDIR to two directories higher
-# Should support symlinks, spaces, etc.
-MIDIR="$(dirname "$(readlink -f "$0")")/../.."
-DIRIN="$MIDIR/data-casa/daily-0.1deg/holding"
+COLTAG="MiCASA_v${VERSION}_flux_x3600_y1800"
+RESTAG="0.1 degree x 0.1 degree"
+FEXT="nc4"
 
-# 2. Output
-ROOTOUT="/css/gmao/geos_carb/pub"
+# Output
 HEADOUT="MiCASA/v$VERSION/netcdf"
 DIROUT="$ROOTOUT/$HEADOUT"					# Form needed for URLs
 HEADDOC="MiCASA/v$VERSION"					# Form needed for URLs
 
-# 3. COG
+# COG
 HEADCOG="MiCASA/v$VERSION/cog"
 DIRCOG="$ROOTOUT/$HEADCOG"					# Copying netCDF form
 
-# 4. Dataportal
+# Dataportal
 SERVE="https://portal.nccs.nasa.gov/datashare/gmao/geos_carb"
 
 # Load NCO utilities on Discover
