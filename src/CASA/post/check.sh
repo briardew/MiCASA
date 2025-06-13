1d60527ea4ca12a571bda0b1ee263fe5513af0b7
Brad Weir <45017314+briardew@users.noreply.github.com>
2025-06-13 04:04:00 -0400

    checksum improvements

--- src/CASA/post/check.sh
+++ src/CASA/post/check.sh
@@ -8,5 +8,8 @@ echo "---"
 DIRCHECK="/css/gmao/geos_carb/pub/MiCASA/v1/netcdf"
 
 while read -r ff; do
-    cd "$(dirname "$ff")" && shasum -a 256 -c "$ff"
+    (
+    cd "$(dirname "$ff")" || exit
+    shasum -a 256 -c "$ff" || exit
+    )
 done <<< "$(find "$DIRCHECK" -name "*_sha256.txt")"
